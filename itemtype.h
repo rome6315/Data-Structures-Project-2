d04839370e4221b01ded4ff9c4ad98fb36546de9
Roman Morasco <92966656+rome6315@users.noreply.github.com>
2023-02-16 15:52:24 +0000

    Update itemtype.h

---
+++ itemtype.h
@@ -0,0 +1,13 @@
+/*This header file is used to define ItemType.  Modify this file to 
+   define the kind of data you wish to place in your linked list.  If the
+   data type does not have == and > operators, you will need to provide
+   overloaded ones, perhaps by using a class for this data type.
+*/
+
+// The following keeps ItemType.h from being included more than once:
+
+
+#include <iostream>
+using namespace std;
+
+typedef float ItemType;
